1d417c9450d7e17bd7fdfd46a8635f7395ff7c27
Valerii Derevshchykov <kaktuz34@gmail.com>
2019-05-21 16:23:12 +0300

    added new pages

--- src/js/components/_dropdown.js
+++ src/js/components/_dropdown.js
@@ -1,4 +1,4 @@
-import {  ACTIVE, OPEN } from '../constants';
+import { BODY, ACTIVE, OPEN } from '../constants';
 
 const drop = $('.js-dropdown');
 drop.each(function() {
@@ -6,10 +6,30 @@ drop.each(function() {
   var dropIcon = this_.find('.js-dropdown-icon');
   var dropText = this_.find('.js-dropdown-text');
   var dropTrigger = this_.find('.js-dropdown-trigger');
-  dropTrigger.on('click', () => {
-    dropText.slideToggle('300');
-    dropIcon.toggleClass(ACTIVE);
-    dropTrigger.toggleClass(OPEN);
+  dropTrigger.on('click', (e) => {
+  	e.preventDefault();
+  	if (!dropTrigger.hasClass(OPEN)) {
+  		dropText.slideDown('300');
+  		dropIcon.addClass(ACTIVE);
+  		dropTrigger.addClass(OPEN);
+  	}
+  	else {
+  		dropText.slideUp('300');
+  		dropIcon.removeClass(ACTIVE);
+  		dropTrigger.removeClass(OPEN);
+  	}
   });
+});
+
+BODY.on('click', e => {
+  if (
+    $(e.target).closest('.js-dropdown-icon').length ||
+    $(e.target).closest('.js-dropdown-text').length ||
+    $(e.target).closest('.js-dropdown-trigger').length
+  )
+    return;
+  $('.js-dropdown-text').slideUp('300');
+  $('.js-dropdown-icon').removeClass(ACTIVE);
+  $('.js-dropdown-trigger').removeClass(OPEN);
 });
 
